5bdccfdea41a92fd54f681089b5d24886c08d89b
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-08-11 11:04:36 +0200

    Implementing Card Items

--- apps/flickr-api/src/main.ts
+++ apps/flickr-api/src/main.ts
@@ -1,28 +1,36 @@
+// Imported axios instead of fetch. Ran through some issues with types
 import axios from 'axios';
 import express from 'express';
+// Global type from libs/shared-types
 import type { FeedResponse } from '@miyagami/shared-types'
 
 const app = express();
 
 app.get('/api', async (req, res) => {
-
+// The original format is XML so we append to the params a key of format with a value
+// of json
   const params = new URLSearchParams()
   params.append("format", "json");
 
   if (typeof req.query.tags === "string") {
+    // if so, append tags to params as well in case there's a query string
     params.append("tags", req.query.tags);
   }
 
   const result = await axios.get('https://www.flickr.com/services/feeds/photos_public.gne', {
     params
   })
-
   if (typeof result.data === 'string') {
     let sanitizedResult = result.data.replace('jsonFlickrFeed(', '');
-
+    
     if (sanitizedResult.at(-1) === ')') {
       sanitizedResult = sanitizedResult.substring(0, sanitizedResult.length - 1)
     }
+    /* 
+    An error kept occurring when parsing the object. By removing the jsonFlickrFeed( 
+    and its closing parenthesis (code aboe) from the response string I could 
+    parse it without problems
+    */
 
     const feed: FeedResponse = JSON.parse(sanitizedResult)
     res.send(feed)
--- libs/shared-types/src/lib/shared-types.ts
+++ libs/shared-types/src/lib/shared-types.ts
@@ -7,19 +7,25 @@ export interface FeedItem {
    * URL of the publication
    */
   link: string
+  /**
+   * Object containing the media URL for the image
+   */
   media: {
     /**
      * URL for the image source
      */
     m: string
   }
-  date_taken: string
-  description: string
-  published: string
+  date_taken?: string
+  description?: string
+  published?: string
   /**
    * String of the author
    */
   author: string
+  /**
+   * Author ID to add to URL query in Link
+   */
   author_id: string
   /**
    * String with tags
